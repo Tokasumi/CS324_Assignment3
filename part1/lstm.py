612c281d2cdb256da0c6c0f509af1aeaf693f3c3
Tokasumi <11912009@mail.sustech.edu.cn>
2022-05-16 22:38:05 +0800

    upload homework files

---
+++ part1/lstm.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+################################################################################
+
+class LSTM(nn.Module):
+
+    def __init__(self, seq_length, input_dim, hidden_dim, output_dim, batch_size):
+        super(LSTM, self).__init__()
+        # Initialization here ...
+
+    def forward(self, x):
+        # Implementation here ...
+        
+    # add more methods here if needed
